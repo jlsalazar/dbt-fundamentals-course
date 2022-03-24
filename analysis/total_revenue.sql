b891ba4c0f40be38139e7487bcd848856ed38b02
Juan Luis Salazar Mendoza <juanluismdz@gmail.com>
2022-03-24 19:23:44 +0000

    analysis and seed - completed and reviewed

--- analysis/total_revenue.sql
+++ analysis/total_revenue.sql
@@ -1,1 +1,12 @@
-select SUM(amount) from {{ref('stg_payments')}} where status='success'
+with payments as (
+select * from {{ ref('stg_payments') }}
+),
+
+aggregated as (
+select
+sum(amount) as total_revenue
+from payments
+where status = 'success'
+)
+
+select * from aggregated
